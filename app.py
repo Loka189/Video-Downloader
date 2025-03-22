e79e626b24fb2dd6b02e23e052f818b282d9dab7
Lokanath Sahoo <lokanath189@gmail.com>
2025-03-22 12:31:35 +0530

    grind20

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import requests
 
@@ -40,4 +41,6 @@ def extract_video_id(url):
     return match.group(1) if match else None
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
